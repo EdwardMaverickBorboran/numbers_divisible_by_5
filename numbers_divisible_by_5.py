6d1f57993c59778ec53eeadb8a385de016cb57e4
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 17:42:32 +0800

    Print the setlist of numbers given.

---
+++ numbers_divisible_by_5.py
@@ -0,0 +1,19 @@
+# Create a program that will --
+
+# Iterate the given list of numbers and print only those numbers which are divisible by 5.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Expected results
+
+# Given list is [10, 20, 33, 46, 55]
+# Divisible by 5
+# 10
+# 20
+# 55
+
+# Set of numbers given
+list = [10, 20, 33, 46, 55]
+print("The given list: ", list)
